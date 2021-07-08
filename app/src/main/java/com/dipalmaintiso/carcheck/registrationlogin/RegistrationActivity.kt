bd1ebdb3b7be7e1aa6a9ab24b8e8ae64d41e871a
Davide Intiso <S4658953@studenti.unige.it>
2021-07-08 17:15:05 +0200

    Gradle build files modified: firebase support added. User class added and packages created

---
+++ app/src/main/java/com/dipalmaintiso/carcheck/registrationlogin/RegistrationActivity.kt
@@ -0,0 +1,25 @@
+package com.dipalmaintiso.carcheck
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+// import com.dave.imessage.messages.LatestMessageActivity
+import com.dipalmaintiso.carcheck.models.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_registration.*
+import java.util.*
+
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
