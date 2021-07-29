3ebc87a5b839c4d8e7400bffab47cf1dc49151ad
Davide Intiso <S4658953@studenti.unige.it>
2021-07-29 17:02:59 +0200

    Vehicle activity/layout created and begun

---
+++ app/src/main/java/com/dipalmaintiso/carcheck/vehiclemanagement/VehicleActivity.kt
@@ -0,0 +1,12 @@
+package com.dipalmaintiso.carcheck.vehiclemanagement
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dipalmaintiso.carcheck.R
+
+class VehicleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vehicle)
+    }
+}
